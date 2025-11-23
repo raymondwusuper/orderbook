96b08b2d6469710970a339102b5939a98df75f27
raymondwusuper <raymond.wu.super@gmail.com>
2025-11-23 11:47:49 -0800

    exhaust engine

--- replay.cpp
+++ replay.cpp
@@ -55,7 +55,7 @@ int main() {
     
     for (auto& t : producers) t.join();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
     engine.stop();
     engineThread.join();
 
